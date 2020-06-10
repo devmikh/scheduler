952e7137bc4f000897bd0f36f91d80775eaa0653
Mikhail Serebriakov <mishacyb@gmail.com>
2020-06-09 19:09:31 -0700

    Implemented Appointment component and created stories for it

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import './styles.scss';
 
-const Appointment = () => {
+import Header from './Header';
+import Show from './Show';
+import Empty from './Empty';
+
+
+const Appointment = (props) => {
   return (
     <article
       className="appointment"
     >
-
+      <Header time={props.time} />
+      { props.interview ?
+        <Show 
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+        :
+        <Empty />
+      }
     </article>
   )
 };
