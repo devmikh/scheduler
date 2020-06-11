6549dba394bbb62fb96c186e6554a965c8e0113c
Mikhail Serebriakov <mishacyb@gmail.com>
2020-06-10 17:29:31 -0700

    Implemented fetching the appointments data from the API server

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,40 +2,10 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
+import { getAppointmentsForDay } from "../helpers/selectors";
 
 import "components/Application.scss";
 
-
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "2pm",
-    interview: {
-      student: "Mikhail Serebriakov",
-      interviewer: {
-        id: 1,
-        name: "Tori Malcolm",
-        avatar: "https://i.imgur.com/Nmx0Qxo.png",
-      }
-    }
-  }
-];
 
 export default function Application(props) {
 
@@ -47,12 +17,14 @@ export default function Application(props) {
 
   const setDay = day => setState({ ...state, day });
 
-  const setDays = days => setState(prev => ({ ...prev, days}));
-
   useEffect(() => {
-    axios.get("/api/days")
-      .then((res) => {
-        setDays(res.data);
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("/api/appointments")
+    ]).then((all) => {
+      console.log(all[0].data);
+      console.log(all[1].data);
+        setState(prev => ({ days: all[0].data, appointments: all[1].data }));
       })
   }, []);
 
@@ -79,7 +51,7 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        { appointments.map(appointment => {
+        { getAppointmentsForDay(state, state.day).map(appointment => {
           return <Appointment
                     key={appointment.id}
                     {...appointment}
