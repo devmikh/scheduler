ecad80d23808c72cce2a275d728c8efd1056a77f
Mikhail Serebriakov <mishacyb@gmail.com>
2020-06-11 14:02:07 -0700

    Implemented useVisualMode hook and tested it

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+
+const useVisualMode = (initial) => {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  const transition = (mode, replace = false) => {
+    const historyArr = [...history];
+    
+    if (replace) {
+      historyArr[historyArr.length - 1] = mode;
+    } else {
+      historyArr.push(mode);
+    }
+
+    setMode(historyArr[historyArr.length - 1]);
+    setHistory(historyArr);
+  }
+
+  const back = () =>  {
+    const historyArr = [...history];
+    if (historyArr.length > 1) {
+      historyArr.pop();
+    }
+    setMode(historyArr[historyArr.length - 1]);
+    setHistory(historyArr);
+  }
+
+  return { mode, transition, back };
+};
+
+export default useVisualMode;
