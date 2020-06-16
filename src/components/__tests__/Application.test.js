01e45788a66df4c4600e6850b48fdd0791a69eef
Mikhail Serebriakov <mishacyb@gmail.com>
2020-06-16 15:17:51 -0700

    Added a test for the Application component

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { waitForElement, fireEvent } from "@testing-library/react";
 
 import { render, cleanup } from "@testing-library/react";
 
@@ -6,6 +7,12 @@ import Application from "components/Application";
 
 afterEach(cleanup);
 
-it("renders without crashing", () => {
-  render(<Application />);
+it("defaults to Monday and changes the schedule when a new day is selected", async () => {
+  const { getByText } = render(<Application />);
+
+  await waitForElement(() => getByText("Monday"))
+  fireEvent.click(getByText("Tuesday"));
+  expect(getByText("Leopold Silvers")).toBeInTheDocument();
+  
 });
+
