bb4625a3f478abc8d71b0ad00a46c3f25ad77999
Mikhail Serebriakov <mishacyb@gmail.com>
2022-06-15 17:05:23 -0700

    Updates

---
+++ precache-manifest.6373be29a1bc941d0bef3d22c5f071ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74d054fc5e64c4c1694d22a285d467d5",
+    "url": "/scheduler/index.html"
+  },
+  {
+    "revision": "d9a37fd97e5ec89ef5da",
+    "url": "/scheduler/static/css/2.e36a74c2.chunk.css"
+  },
+  {
+    "revision": "78f5f90c7c518a949ef8",
+    "url": "/scheduler/static/css/main.a7b16c19.chunk.css"
+  },
+  {
+    "revision": "d9a37fd97e5ec89ef5da",
+    "url": "/scheduler/static/js/2.2cac72e5.chunk.js"
+  },
+  {
+    "revision": "78f5f90c7c518a949ef8",
+    "url": "/scheduler/static/js/main.b7cc72e9.chunk.js"
+  },
+  {
+    "revision": "b53e8256284673f4aa63",
+    "url": "/scheduler/static/js/runtime~main.833ed90b.js"
+  }
+]);
